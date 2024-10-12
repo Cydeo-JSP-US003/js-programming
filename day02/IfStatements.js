a2e84849bf24fe259ca1f7ab9d78c4611997cba4
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-10-12 10:39:42 -0400

    Day02

---
+++ day02/IfStatements.js
@@ -0,0 +1,46 @@
+let score = 50;
+
+if(score >= 60){
+    console.log("Congrats, You passed the exam!");
+}else{
+    console.log("Sorry, You failed the exam!");
+}
+
+console.log("==================================");
+
+
+let number = 100;
+
+if(number > 0){
+    console.log("Positive");
+}else if(number < 0 ){
+    console.log("Negative");
+}else{
+    console.log("Zero");
+}
+
+console.log("==================================");
+
+
+score = 300;
+
+if(score >= 0 && score <= 100){
+    
+    if(score >= 60){
+        console.log("Congrats, You passed the exam!");
+    }else{
+        console.log("Sorry, You failed the exam!");
+    }
+
+}else{
+    console.log("Invalid score");
+}
+
+
+
+
+
+
+
+
+
