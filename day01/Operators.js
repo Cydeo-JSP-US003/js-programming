de370d2c0558502bfb6236d16d6cabe9be1db891
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-10-12 09:16:39 -0400

    Day01

---
+++ day01/Operators.js
@@ -0,0 +1,34 @@
+console.log(10 + 5);
+console.log(20 - 5);
+console.log(20 * 2);
+console.log(10 / 2);
+
+console.log("----------------------");
+
+let a = 20;
+
+a = 40;
+
+console.log(a);
+
+a += 100;
+
+console.log(a);
+
+a -= 50;
+
+console.log(a);
+
+console.log("--------------------------------");
+
+console.log(10 > 5);
+console.log(10 >= 10);
+
+console.log(10 < 5);
+
+console.log(100 == 100);
+console.log(100 == "100"); // equal: ignores the data type of the values
+console.log(100 === "100");
+
+console.log( 100 != "100");// not equal: ignores the data type of the values
+console.log(100 !== "100");
---
+++ day01/Variables.js
@@ -0,0 +1,48 @@
+console.log("Hello Wolrd!");
+console.log("Hello CYDEO!");
+
+console.log("------------------------------------");
+
+let schoolName = "CYDEO";
+
+console.log(schoolName);
+
+// let schoolName = 'MIT';
+
+console.log(schoolName);
+
+console.log("------------------------------------");
+
+if (5 > 4) {
+  //let a = 100;
+  var a = 100;
+}
+
+console.log(a);
+
+console.log("------------------------------------");
+
+const NUMBER_OF_HOURS_IN_A_DAY = 24;
+
+// number_of_hours_in_a_day = 28;
+
+console.log(NUMBER_OF_HOURS_IN_A_DAY);
+
+console.log("------------------------------------");
+
+let student_name = "Muhtar";
+
+console.log(student_name);
+
+let score = 98;
+
+console.log(score);
+
+let gpa = 3.5;
+
+console.log(gpa);
+
+// let a;
+
+console.log(typeof gpa);
+console.log(typeof student_name);
