35ff1fc000782c60703f540319e6a2946e61a036
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-10-12 12:28:13 -0400

    Day02

---
+++ day02/ArraysPractice.js
@@ -0,0 +1,24 @@
+let elements = ["Java", "Python", 12, 13, 14, true, false];
+
+console.log(elements);
+
+console.log("-------------------------------------");
+
+let students = ["Kibriyo", "Nessar", "Daur", "Aysen"];
+
+console.log(students);
+console.log(students.length);
+
+students.push("Muhtar");
+students.push("Kasim");
+
+console.log(students);
+console.log(students.length);
+
+students[students.length -2] = "Mehmet";
+
+console.log(students);
+
+console.log(students[0]);
+
+// come back at 12:40 PM
