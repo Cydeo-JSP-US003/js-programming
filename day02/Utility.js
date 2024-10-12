5f6ad62aaa58e944c9013ad991cc008a9811d540
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-10-12 13:18:59 -0400

    Day02

---
+++ day02/Utility.js
@@ -0,0 +1,24 @@
+function square(x){
+    return x * x;
+}
+
+function cube(y){
+    return y * y * y;
+}
+
+function calculate(num1, num2, operator) {
+    if (operator === '-') {
+        return num1 - num2;
+    } else if (operator === '+') {
+        return num1 + num2;
+    } else if (operator === '*') {
+        return num1 * num2;
+    } else if (operator === '/') {
+        return num1 / num2;
+    } else {
+        console.log('Invalid operator');
+        return 0;
+    }
+}
+
+module.exports = { square, cube, calculate}
